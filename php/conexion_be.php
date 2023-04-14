3a3a16d142579f4762e3fb1638088a9fad196f0a
Erik Tamayo <erik.tamayohe@uanl.edu.mx>
2023-04-14 12:24:07 -0600

    Create conexion_be.php

---
+++ php/conexion_be.php
@@ -0,0 +1,13 @@
+<?php
+
+$conexion = mysqli_connect("localhost", "root", "", "login_register_db");
+
+/*Verificar que la base de datos este conectada
+if ($conexion){
+    echo "conectado";
+}else{
+        echo "no conectado";
+}*/
+
+
+?>
