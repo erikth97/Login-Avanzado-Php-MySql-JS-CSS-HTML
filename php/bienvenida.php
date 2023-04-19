2f492445a411991c3639f1de837cc130d383dc4b
Erik Tamayo <erik.tamayohe@uanl.edu.mx>
2023-04-19 13:42:25 -0600

    Create bienvenida.php

---
+++ php/bienvenida.php
@@ -0,0 +1,31 @@
+<?php
+
+    session_start();
+
+    if(!isset($_SESSION['usuario'])){
+        echo '
+            <script>
+            alert("Debes de Iniciar Sesion");
+            window.location = "index.php";
+            </script>
+        ';
+        session_destroy();
+        die();
+    }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h1>Bienvenido |  Inicio</h1>
+    <a href=" cerrar_sesion.php">LogOut</a>
+</body>
+</html>
